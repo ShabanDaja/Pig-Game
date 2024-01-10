776140f84edc12a327cb05e717c0ec22fb85199c
Shaban Daja <shabandaja710@gmail.com>
2024-01-10 15:10:17 +0100

    Resetting the game. (player wins with 20 score)

--- script.js
+++ script.js
@@ -10,17 +10,30 @@ const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+const resetBtn = document.querySelector('.btn--new');
 
 // Starting conditions
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+  player0El.classList.add('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -69,3 +82,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+resetBtn.addEventListener('click', init);
